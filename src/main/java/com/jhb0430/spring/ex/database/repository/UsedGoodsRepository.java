459ec976dec09e52013a664cc4853a56ef6b16e9
jhb0430 <leky2224@gmail.com>
2024-11-03 23:50:21 +0900

    [배지현] - 주말 과제 도중

--- src/main/java/com/jhb0430/spring/ex/database/repository/UsedGoodsRepository.java
+++ src/main/java/com/jhb0430/spring/ex/database/repository/UsedGoodsRepository.java
@@ -1,16 +1,15 @@
 package com.jhb0430.spring.ex.database.repository;
 
 import java.util.List;
-
 import org.apache.ibatis.annotations.Mapper;
-
 import com.jhb0430.spring.ex.database.domain.UsedGoods;
 
-// 데이터베이스 관련 처리
-// Mybatis FrameWork
+//데이터베이스 관련 처리
+//Mybatis FrameWork
+//	src/main/resources/mappers/*Mapper.xml 
+//(어쩌구저쩌구 다 적어도 마지막은 Mapper.xml로 끝나야한다  )
 @Mapper
-//	src/main/resources/mappers/*Mapper.xml (어쩌구저쩌구 다 적어도 마지막은 Mapper.xml로 끝나야한다  )
-public class UsedGoodsRepository {
+public interface UsedGoodsRepository {
 
 	// used_goods 테이블 모든 행 조회
 	public List<UsedGoods> selectUsedGoodsList();
