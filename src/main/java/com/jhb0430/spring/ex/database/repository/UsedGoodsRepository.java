aaccfbe527a37bd7aa09bc4f2a1c66c18884e838
jhb0430 <leky2224@gmail.com>
2024-11-04 13:02:11 +0900

    [배지현] - 예제 오류 처리

--- src/main/java/com/jhb0430/spring/ex/database/repository/UsedGoodsRepository.java
+++ src/main/java/com/jhb0430/spring/ex/database/repository/UsedGoodsRepository.java
@@ -1,7 +1,9 @@
 package com.jhb0430.spring.ex.database.repository;
 
 import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
 import com.jhb0430.spring.ex.database.domain.UsedGoods;
 
 //데이터베이스 관련 처리
