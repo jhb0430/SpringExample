71477e25e8aa6b9f09b1c1a78f1be5701bad4f91
jhb0430 <leky2224@gmail.com>
2024-11-22 18:52:03 +0900

    [배지현] - jpa 2.

--- src/main/java/com/jhb0430/spring/ex/jpa/StudentController.java
+++ src/main/java/com/jhb0430/spring/ex/jpa/StudentController.java
@@ -1,5 +1,8 @@
 package com.jhb0430.spring.ex.jpa;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.jhb0430.spring.ex.jpa.domain.Student;
+import com.jhb0430.spring.ex.jpa.repository.StudentRepository;
 import com.jhb0430.spring.ex.jpa.service.StudentService;
 
 @RequestMapping("/jpa/student")
@@ -15,6 +19,20 @@ public class StudentController {
 
 	@Autowired
 	private StudentService studentService;
+	
+	
+	
+	// 원래는 절대 이렇게 하면 안됨! 절대!! Controller에서 Repository 활용 금지~!~!~!~!
+	// 지금은 임시로 사용하는 거임
+	@Autowired
+	private StudentRepository studentRepository;
+	
+	
+	
+	
+	
+	
+	
 	
 	// create, update, delete
 	
@@ -52,7 +70,43 @@ public class StudentController {
 	}
 	
 	
-	
+	@ResponseBody
+	@GetMapping("/find")
+	public List<Student> findStudent(){
+		
+		List<Student> studentList = null;
+		
+		// 모든 행 조회
+//		List<Student> studentList = studentRepository.findAll();
+		
+		// 모든 행을 id 컬럼 내림 차순으로 조회
+//		studentList = studentRepository.findAllByOrderByIdDesc();
+		
+		// id 기준으로 내림차순 정렬된 결과를 2개만 조회
+//		studentList = studentRepository.findTop2ByOrderByIdDesc();
+		
+		// 전달받은 이름과 일치하는 행 조회
+		studentList = studentRepository.findByName("김어쩌구");
+		
+		List<String> nameList = new ArrayList<>();
+		nameList.add("유재석");
+		nameList.add("조세호");
+		
+		studentList = studentRepository.findByNameIn(nameList);
+		
+		// 전달 받은 키워드가 포함된 email 컬럼을 가진 행 조회
+		studentList = studentRepository.findByEmailContaining("gmail");
+		
+		// id 컬럼 값이 특정한 값들 사이에 포함되는 행을 id 기반으로 내림차순 해서 조회
+//		studentList = studentRepository.findByIdBetweenOrderByIdDesc(1, 3);
+		
+		// 쿼리를 직접 작성
+		// Native Query
+		studentList = studentRepository.selectBYDreamJob("모델");
+		
+		
+		return studentList;
+	}
 	
 	
 	
--- src/main/java/com/jhb0430/spring/ex/jpa/repository/StudentRepository.java
+++ src/main/java/com/jhb0430/spring/ex/jpa/repository/StudentRepository.java
@@ -1,11 +1,50 @@
 package com.jhb0430.spring.ex.jpa.repository;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.jhb0430.spring.ex.jpa.domain.Student;
 
 //														<entity클래스,해당하는 클래스의 프라이머리 키 타입>
 public interface StudentRepository extends JpaRepository<Student,Integer>{
 
+	// id를 기준으로 내림차순 정렬된 결과 조회
+	// ORDER BY `id` DESC
+	public List<Student> findAllByOrderByIdDesc();
+	
+	// id 기준으로 내림차순 정렬된 결과를 2개만 조회
+	// ORDER BY `id` DESC LIMIT 2
+	
+	public List<Student > findTop2ByOrderByIdDesc();
+
+	// 전달받은 이름과 일치하는 행 조회
+	// WHERE `name` = #{name}
+	
+	public List<Student > findByName(String name);
+	
+
+	// 전달 받은 이름들과 일치하는 행 조회
+	// WHERE `name` IN(#{},#{})
+	public List<Student >findByNameIn(List<String> nameList);
+	
+	// 전달 받은 키워드가 포함된 email 컬럼을 가진 행 조회
+	// WHERE `email` LIKE %gmail% '%${keyword}%'
+	public List<Student> findByEmailContaining(String keyword);
+	
+	
+	// id 컬럼 값이 특정한 값들 사이에 포함되는 행을 id 기반으로 내림차순 해서 조회
+	// WHERE `id` BETWEEN 1 AND 3 ORDER BY `id` DESC;
+	public List<Student> findByIdBetweenOrderByIdDesc(int start, int end);
+	
+	
+	// 쿼리를 직접 작성
+	// Native Query
+	// dreamJob 일치하는 행 조회
+	@Query(value="SELECT * FROM `new_student` WHERE `dreamJob`= : dreamJob" ,nativeQuery= true)
+	public List<Student> selectBYDreamJob(@Param("dreamJob")String dreamjob);
+	
 	
 }
